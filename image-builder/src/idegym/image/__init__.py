290356a63f5b1e88ce07ea25413c06ee8a1360b7
Ozren Dabić <ozren.dabic@jetbrains.com>
2026-04-20 19:29:08 +0200

    [JBRes-9012] CI changes (#74)

--- image-builder/src/idegym/image/__init__.py
+++ image-builder/src/idegym/image/__init__.py
@@ -11,8 +11,25 @@ Core types:
 Built-in plugins are in ``idegym.image.plugins`` and are auto-registered on import of this package.
 """
 
+from importlib.metadata import PackageNotFoundError, version
+
 from idegym.api.docker import BaseImage as Base
 from idegym.image.builder import Image
 from idegym.image.plugin import BuildContext, PluginBase, image_plugin
 
-__all__ = ("Base", "BuildContext", "Image", "PluginBase", "image_plugin")
+try:
+    __version__ = version("idegym-image-builder")
+except PackageNotFoundError:
+    __version__ = "0.0.0.dev0"
+
+if __version__ == "0.0.0.dev0":
+    __version__ = "latest"
+
+__all__ = (
+    "__version__",
+    "Base",
+    "BuildContext",
+    "Image",
+    "PluginBase",
+    "image_plugin",
+)
